c0041a0df13072b7e58d852864ee725458daa0db
Domenico Vacchiano <info info@domenicovacchiano.com>
2022-08-24 17:54:16 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo512.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Great! This seems working! <br/>Let's start have fun now :)</p>
+        <a
+          className="App-link"
+          href="https://www.swintt.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn About Swintt
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
